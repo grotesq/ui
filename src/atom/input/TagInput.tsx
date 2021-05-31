5a25dd8a4a2098a9c947ccc42e80aed288585468
Park June Chul <pjc0247@naver.com>
2021-05-31 13:28:37 +0900

    fix: TagInput

--- src/atom/input/TagInput.tsx
+++ src/atom/input/TagInput.tsx
@@ -2,23 +2,40 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 import { Tag } from 'atom/tag';
+import { Space } from 'atom/layout';
 
 type TagInputProps = {
+  gap?: number;
   items: string[];
   value: string[];
   onChange: OnChangeCallback<string[]>;
 };
 export const TagInput = ({
+  gap = 8,
   items,
   value,
   onChange,
 }: TagInputProps) => {
+  const onPressTag = (tag: string) => {
+    if (value.includes(tag))
+      onChange(value.filter(x => x !== tag));
+    else
+      onChange([...value, tag]);
+  };
+
   return (
     <Container>
       {items.map(x => (
-        <Tag>
-          {x}
-        </Tag>
+        <React.Fragment
+          key={x}
+        >
+          <Tag
+            onPress={() => onPressTag(x)}
+          >
+            {x}
+          </Tag>
+          <Space width={gap} />
+        </React.Fragment>
       ))}
     </Container>
   );
